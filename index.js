dad2eea2ce4dbeb4a1f3dc6646105c0f286d219b
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2023-03-05 18:32:26 +0000

    added createmanager function

--- index.js
+++ index.js
@@ -13,3 +13,70 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const idList = []
+const teamMembers = []
+
+const appMenu = () => {
+    function createManager () {
+        console.log("Build your team");
+        inquirer.prompt ([
+            {
+                type: "input",
+                name: "managerName",
+                message: `What is the team manager's name?`,
+                validate: answer => {
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "Please enter at least one character."
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: `What is the team manager's id?`,
+                validate: answer => {
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "Please enter team manager's id."
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: `What is the team manager's email?`,
+                validate: answer => {
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "Please enter team manager's email address."
+                }
+
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: `What is the team manager's office number?`,
+                validate: answer => {
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "Please enter team manager's office number."
+                }
+            },
+
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamMembers.push(manager);
+            idList.push(answers.managerId);
+        })
+    }
+
+    createManager();
+
+
+}
+
+
+appMenu();
